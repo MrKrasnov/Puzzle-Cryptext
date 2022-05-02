91ff1ea41de6ea8d41683f79148a1bb76e4fe898
MrKrasnov <mr.krasnov.y@gmail.com>
2022-05-02 10:36:26 +0300

    1.1 has updated data and fix code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# criptGAME.github.io
- 
+# Puzzle-Cryptext
+Popularized by the 2003 release of The DaVinci Code by Dan Brown, the Cryptex has had world-wide appeal to avid puzzle collectors.
+Coined from the combination of the words cryptology and codex, its use of “the science of cryptology” secures and 
+protects secrets written on parchment contained within. 
+
+The Cryptex functions similar to a bicycle combination lock. Discs must be positioned in the exact manner to spell out the correct password.
+Tumblers inside align allowing the inner cylinder to slide apart, protecting the contents in the inner space.
+In the story of the DaVinci code, the incorrect combination will release a vial of vinegar, destroying the message inside, making it unreadable.
--- main.js
+++ main.js
@@ -1,50 +1,46 @@
 const img = ['one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eit', 'nine'];
-let square = document.querySelectorAll('.square');
-console.log(square);
-// генератор чисел
-function random(min, max){
-    return Math.round(min - 0.5+Math.random()*(max-min+1));
+let photo = document.querySelectorAll('.square');
+let winnerDiv = document.querySelector('#out');
+
+function random(min, max) {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
 
-square.forEach(function(element){
-    // перебираем цифры в массиве
-    let randNum = random(0, img.length -1);
-    console.log(randNum);
-    element.style.backgroundImage = 'url(img/'+img[randNum]+'.jpg)';
-    // индекс изображения
-    element.dataset.data = randNum;
-    element.onclick = changeImage;
+photo.forEach(function (element) {
+    let int = random(0, img.length - 1);
 
+    element.style.backgroundImage = 'url(img/' + img[int] + '.jpg)';
+    element.dataset.data = int;
+
+    element.onclick = changeImage;
 });
-// проверка изображений при клике
-function changeImage(){
-    document.querySelector('#out').innerHTML = '';
+
+function changeImage() {
+    winnerDiv.innerHTML = 'Всё ещё комбинация не правильная!';
+    winnerDiv.style.fontSize = '16px';
+
     let data = this.dataset.data
-    console.log(data);
-    if(+data+1 < img.length){
+
+    if (+data + 1 < img.length) {
         data++;
     }
-    else{
+    else {
         data = 0;
     }
     this.dataset.data = data;
-    this.style.backgroundImage = 'url(img/'+img[data]+'.jpg)';
+    this.style.backgroundImage = 'url(img/' + img[data] + '.jpg)';
+
     checkPrize();
 }
 
 function checkPrize() {
-    // проверка массивных цифр всех трёх изображений
     let res = [];
-    square.forEach(function(element){
+    photo.forEach(function (element) {
         res.push(element.dataset.data);
     });
-    console.log(res);
-    // еиволсу еондебоп
-    if (res.join('') == '8888'){
-        document.querySelector('#out').innerHTML = "Мо";
-    } else if (res.join('') == '3156'){
-        document.querySelector('#out').innerHTML = "ло";
-    } else if (res.join('') == '7584'){
-        document.querySelector('#out').innerHTML = "дец";
+
+    if (res.join('') == '7584') {
+        winnerDiv.style.fontSize = '25px';
+        winnerDiv.innerHTML = "Ты подобрал комбинацию!";
     }
 }
